b75c5bc3d84315090246c01e100f1dc6ed4aeebe
mehdi1-dev-1 <fardoussmahdi66@gmail.com>
2026-09-01 10:32:28 +0100

    challenge3

---
+++ challenge3.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface ({
+    input : process.stdin,
+    output : process.stdout
+});
+rl.question("Entrer la valeur en GB : " , (answer) => {
+let gb = Number(answer);
+let mb = gb * 1024;
+console.log("resultat est :" + mb + "  MB" );
+rl.close()
+});
