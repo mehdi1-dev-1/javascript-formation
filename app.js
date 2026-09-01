2e5e8f390938e4b1f8c7b0dfad1f4b18d06a9584
mehdi1-dev-1 <fardoussmahdi66@gmail.com>
2026-09-01 09:21:58 +0100

    chalenge1

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Bonjour !");
+console.log("nom:mehdi");
+console.log("prenom:fardouss")
+    const nom = "mehdi";
+    const prenom = "fardouss";
+    let age = 19 ;
+    age = 30;
+    age = 40;
+    const pays = "maroc";
+    let score = 0;
+    score = 10;
+    score = score + 20;
+    console.log("nom:" + nom);
+    console.log("prenom:" + prenom);
+    console.log("age:" + age);
+    console.log("pays:" + pays);
+    console.log("score:" + score);
